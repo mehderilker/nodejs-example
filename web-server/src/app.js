f8aaf284bf7c71992226fb2bbbecba09ab4f36bb
Ilker MEHDER <yasinilker67@gmail.com>
2020-10-01 07:45:26 +0300

    advanced template

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,30 +1,42 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
 
+//Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+// Setup handlebars engine and views location
 app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+//Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res) => {
     res.render('index',{
-        title:'Weather'
+        title:'Weather',
+        name:'ilker'
     })
 })
-
 
 // Routers = main / help / about 
 
 app.get('/about', (req,res) => {
     res.render('about', {
-        title:'About Page'
+        title:'About Page',
+        name:'ilker'
     } )
 })
 
 app.get('/help',(req,res)=> {
-    res.send('HelPage')
+    res.render('help',{
+        title:'Help Page',
+        name:'ilker'
+    })
 })
 
 app.listen(3000, () => {
