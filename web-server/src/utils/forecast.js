101bd67b42b85952eb8db13f814a92ea93c7803b
Ilker MEHDER <yasinilker67@gmail.com>
2020-10-14 11:17:04 +0300

    weather connected/json

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -1,15 +1,16 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.openweathermap.org/data/2.5/weather?lat='+latitude+'&lon='+longitude+'&appid=79d85219f0eae0d73a5c9484d1581556'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, {body} ) => {
+        
         if (error) {
-            callback('Unable to connect to weather service!', undefined)
+            callback('Unable connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.name + ' It is currently ' + body.weather[0].description + ' degress out. There is a ' + body.wind.deg + '% chance of rain.')
         }
     })
 }
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoiaGV5ZHJpY2giLCJhIjoiY2tmeGhpcWtoMDBqazJybnpmbzZkYWhmZyJ9.LrT0IU8AwmnRT4SLSHn7tQ&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
